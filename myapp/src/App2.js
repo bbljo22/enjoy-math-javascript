921fb31e59004237e0a30a4d2af78953ace8b864
bbljo22 <bbljo2@yahoo.com>
2024-05-19 12:39:56 +0900

    new branch mongodb

---
+++ myapp/src/App2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GeoLocation from './components/GeoLocation';
+
+
+const App = () => {
+  return (
+    <div>
+      <GeoLocation />
+    </div>
+  );
+};
+
+export default App;
